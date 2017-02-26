74220c41797c0b93e46e72f3ceb1b8442b4bbad3
echizenyayota <virtualmisso30@gmail.com>
2017-02-26 15:01:57 +0900

    P32. プラグインの準備

---
+++ wp-content/plugins/wpwa-user-manager/class-wpwa-user-manager.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  Plugin Name: WPWA User Manager
+  Plugin URI:
+  Description: User management module for the portfolio management application.
+  Author: Rakhitha Nimesh
+  Version: 1.0
+  Author URI: http://www.innovativephp.com/
+*/
+
+class WPWA User_Manager {
+  public function __construct() {
+    // 初期化コード
+  }
+}
+
+$user_manage = new WPWA_User_Manager();
