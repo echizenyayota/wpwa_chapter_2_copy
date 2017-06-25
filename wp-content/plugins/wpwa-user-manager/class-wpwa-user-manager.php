4e3aeb4c7cc4c26b5bee757303755bd6bf886db6
echizenyayota <virtualmisso30@gmail.com>
2017-06-25 12:45:11 +0900

    P33.ユーザーロールの基礎

--- wp-content/plugins/wpwa-user-manager/class-wpwa-user-manager.php
+++ wp-content/plugins/wpwa-user-manager/class-wpwa-user-manager.php
@@ -12,6 +12,30 @@ class WPWA_User_Manager {
   public function __construct() {
     // 初期化コード
   }
+
+  //    /*
+  //     * Add extra validation on user registration
+  //     *
+  //     * @param  -
+  //     * @return void
+  //     */
+  //    public function validate_user() {
+  //        remove_action('wpwa_register_user', array($this, 'register_user'));
+  //    }
+
+      /*
+       * Add new user roles to application on activation
+       *
+       * @param  -
+       * @return void
+      */
+
+      public function add_application_user_roles() {
+          add_role( 'follower', 'Follower', array( 'read' => true ) );
+          add_role( 'developer', 'Developer', array( 'read' => true ) );
+          add_role( 'member', 'Member', array( 'read' => true ) );
+      }
+      
 }
 
 $user_manage = new WPWA_User_Manager();
